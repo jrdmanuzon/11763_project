ac607e76a8c59ffad9b159c6a852dc74fc73020f
June <jmanuzon@abo.fi>
2024-05-14 23:21:19 +0200

    Add initial code and add temporary CT image

--- main.py
+++ main.py
@@ -9,9 +9,6 @@ from skimage import transform
 # ● The segmentation image is resliced according to the dicom headers.
 # ● The four regions of interest appear on a segmentation (i.e. label image).
 
-def load_dcm(filepath: str):
-    """ Load a DICOM file. """
-    return pydicom.dcmread(filepath)
 
 def load_dicom_image(file_path):
     """
@@ -20,13 +17,6 @@ def load_dicom_image(file_path):
     dcm = pydicom.dcmread(file_path)
     return dcm.pixel_array
 
-def reslice_segmentation(seg_array, seg_spacing, seg_thickness, ct_spacing, ct_thickness, ct_shape):
-    """
-    Reslice segmentation image according to CT image header.
-    """
-    resize_factor = (ct_spacing[0] / seg_spacing[0], ct_spacing[1] / seg_spacing[1], ct_thickness / seg_thickness)
-    resliced_seg = transform.resize(seg_array, ct_shape, order=0)
-    return resliced_seg
 
 def plot_images(ct_image, seg_image):
     """
@@ -47,21 +37,8 @@ def process_images(ct_file_path, seg_file_path):
     """
     ct_array = load_dicom_image(ct_file_path)
     seg_array = load_dicom_image(seg_file_path)
-    
-    # Extract metadata from DICOM headers
-    ct_dcm = pydicom.dcmread(ct_file_path)
-    seg_dcm = pydicom.dcmread(seg_file_path)
-    ct_spacing = ct_dcm.PixelSpacing
-    ct_thickness = ct_dcm.SliceThickness
-    seg_spacing = seg_dcm.PixelSpacing
-    seg_thickness = seg_dcm.SliceThickness
-    ct_shape = np.shape(ct_array)
-    
-    # Reslice segmentation image
-    resliced_seg = reslice_segmentation(seg_array, seg_spacing, seg_thickness, ct_spacing, ct_thickness, ct_shape)
-    
-    # Plot images
-    plot_images(ct_array, resliced_seg)
+
+    plot_images(ct_array, seg_array)
 
 
 # Rotating MIP (20%)
@@ -85,8 +62,7 @@ def process_images(ct_file_path, seg_file_path):
 # space (i.e. the patient space).
 # ● The thalamus mask has been visualized in the input space.
 
-
-# Example usage
-ct_file_path = "ct_image.dcm"
-seg_file_path = r"HCC_006\08-06-1999-NA-ABDPEL LIVER-65146\300.000000-Segmentation-58134\1-1.dcm"
+# temporary files, need to confirm correct ones, currently errors
+ct_file_path = "HCC_006/08-06-1999-NA-ABDPEL LIVER-65146/2.000000-PRE LIVER-29453/1-01.dcm"
+seg_file_path = "HCC_006/08-06-1999-NA-ABDPEL LIVER-65146/300.000000-Segmentation-58134/1-1.dcm"
 process_images(ct_file_path, seg_file_path)
